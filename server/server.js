83dc8ecdad8cfc5410f3a2cd321062eb8826c7ba
Arslan <arslan.akram135@gmail.com>
2018-04-12 04:01:31 +0500

    connect with web socket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname , '../public');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname , '../public');
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,function(){
+io.on('connection',function(socket){
+    console.log('New User connected');
+
+    socket.on('disconnect',function(){
+        console.log('User was disconnected');
+    });
+});
+
+server.listen(port,function(){
     console.log('Server is up on port ',port);
 });
 
